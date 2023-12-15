81d0d3d1de8041fc10b2524d6f51bd54f0bdefd1
DanielKirshner <danielkirshner7878@gmail.com>
2023-12-16 00:39:41 +0200

    Added print_board method

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include <vector>
+
+void print_board(const std::vector<std::vector<int32_t>>& board)
+{
+    for (size_t i = 0; i < board.size(); ++i)
+    {
+        const std::vector<int32_t>& row = board[i];
+
+        for (size_t j = 0; j < row.size(); ++j)
+        {
+            int32_t cell = row[j];
+            std::cout << cell << " ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+}
 
 int32_t main()
 {
     try
     {
+        std::vector<std::vector<int32_t>> board = {};
+        
         return EXIT_SUCCESS;
     }
     catch (const std::exception& exception)
